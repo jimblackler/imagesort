15354c6ae7cc1edc00013c2729815801547c03cf
Jim Blackler <jimblackler@gmail.com>
2019-04-26 09:44:32 +0100

    Support recursion

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from PIL import Image
 
 path = sys.argv[1]
 
-for f in os.listdir(path):
-    full = os.path.join(path, f)
-    wide_dir = os.path.join(path, 'wide')
-    tall_dir = os.path.join(path, 'tall')
+for root, subdirs, files in os.walk(path):
+    split_path = os.path.normpath(root).split(os.sep)
+    if split_path[-1] in ['wide', 'tall']:
+        continue
+    for f in files:
+        full = os.path.join(root, f)
+        try:
+            if os.path.isfile(full):
+                with Image.open(full) as image:
+                    if image.size[0] > image.size[1]:
+                        use_dirname = 'wide'
+                    else:
+                        use_dirname = 'tall'
 
-    if os.path.isfile(full):
-        with Image.open(full) as image:
-            if image.size[0] > image.size[1]:
-                use_dir = wide_dir
-            else:
-                use_dir = tall_dir
+                use_dir = os.path.join(root, use_dirname)
 
-        if not os.path.exists(use_dir):
-            os.mkdir(use_dir)
-        new_location = os.path.join(use_dir, f)
-        os.rename(full, new_location)
+                if not os.path.exists(use_dir):
+                    os.mkdir(use_dir)
+                new_location = os.path.join(use_dir, f)
+                print full + ' -> ' + new_location
+                os.rename(full, new_location)
+        except IOError as e:
+            print e
